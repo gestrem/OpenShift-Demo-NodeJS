bb41a9f46cf1bd7528493a41bc735b8132b276be
gestrem <gestrem@redhat.com>
2019-04-30 16:18:00 +0200

    add set-projects.sh

---
+++ set-projects.sh
@@ -0,0 +1,14 @@
+oc new-project demo-dev
+oc new-project demo-build
+oc new-project demo-test
+oc new-project demo-prod
+
+#build JENKINS app
+
+oc new-app -n demo-build --template=jenkins-persistent --name=jenkins -p MEMORY_LIMIT=1Gi
+
+oc process -f setup/all-in-one-template.yaml TEST_ROUTE_HOSTNAME=demo-test.ge-apps.openhybridcloud.io PROD_ROUTE_HOSTNAME=demo-prod.ge-apps.openhybridcloud.io > objects.json
+
+oc apply -f objects.json
+
+
