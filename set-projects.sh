901a8c8db96a8541089e2e9b0135a63975b3e867
gestrem <gestrem@redhat.com>
2019-05-29 18:04:11 +0200

    update gitignore

--- set-projects.sh
+++ set-projects.sh
@@ -7,7 +7,7 @@ oc new-project demo-prod
 
 oc new-app -n demo-build --template=jenkins-persistent --name=jenkins -p MEMORY_LIMIT=1Gi
 
-oc process -f setup/all-in-one-template.yaml TEST_ROUTE_HOSTNAME=demo-test.192.168.64.3.nip.io PROD_ROUTE_HOSTNAME=ddemo-prod.192.168.64.3.nip.io > objects.json
+oc process -f setup/all-in-one-template.yaml TEST_ROUTE_HOSTNAME=demo-test.ge-apps.guillaume.openhybridcloud.io PROD_ROUTE_HOSTNAME=demo-prod.ge-apps.guillaume.openhybridcloud.io > objects.json
 
 oc apply -f objects.json
 
